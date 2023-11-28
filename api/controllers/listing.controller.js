51c32fefd556b8b0ca65926705dad8dce8d46ebd
Satender singh <75835926+Satender1998@users.noreply.github.com>
2023-11-28 13:30:51 +0530

    v11

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -28,26 +28,6 @@ export const deleteListing = async (req, res, next) => {
   }
 };
 
-// export const updateListing = async (req, res, next) => {
-//   const listing = await Listing.findById(req.params.id);
-//   if (!listing) {
-//     return next(errorHandler(404, "Listing not found!"));
-//   }
-//   if (req.user.id !== listing.userRef) {
-//     return next(errorHandler(401, "You can only update your own listings!"));
-//   }
-
-//   try {
-//     const updatedListing = await Listing.findByIdAndUpdate(
-//       req.params.id,
-//       req.body,
-//       { new: true }
-//     );
-//     res.status(200).json(updatedListing);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
 
 
 
@@ -73,10 +53,8 @@ export const updateListing = async (req, res, next) => {
       { new: true }
     );
 
-    // Respond with the updated listing
     res.status(200).json(updatedListing);
   } catch (error) {
-    // Handle any errors that occurred during the update process
     next(error);
   }
 };
--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -38,30 +38,42 @@ export const updateUser = async (req, res, next) => {
   }
 };
 
+
+
 export const deleteUser = async (req, res, next) => {
-  if (req.user.id !== req.params.id)
+  const { id: userId } = req.user;
+  const { id: paramsId } = req.params;
+
+  if (userId !== paramsId) {
     return next(errorHandler(401, "You can only delete your own account!"));
+  }
+
   try {
-    await User.findByIdAndDelete(req.params.id);
-    res.clearCookie("access_token");
-    res.status(200).json("User has been deleted!");
+    await User.findByIdAndDelete(paramsId);
+    res.clearCookie("access_token").status(200).json("User has been deleted!");
   } catch (error) {
     next(error);
   }
 };
 
+
 export const getUserListings = async (req, res, next) => {
-  if (req.user.id === req.params.id) {
-    try {
-      const listings = await Listing.find({ userRef: req.params.id });
-      res.status(200).json(listings);
-    } catch (error) {
-      next(error);
-    }
-  } else {
+  const { id: userId } = req.user;
+  const { id: paramsId } = req.params;
+
+  if (userId !== paramsId) {
     return next(errorHandler(401, "You can only view your own listings!"));
   }
+
+  try {
+    const listings = await Listing.find({ userRef: paramsId });
+    res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
 };
+
+
 
 export const getUser = async (req, res, next) => {
   try {
